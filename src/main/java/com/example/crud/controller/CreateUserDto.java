cea86ec30f2edf1d93cb664a5881fa7fe888d470
vitoralveschaves <vitorchavesalv@gmail.com>
2024-07-15 01:10:11 -0300

    feat: add CreateUserDto and UpdateUserDto records

---
+++ src/main/java/com/example/crud/controller/CreateUserDto.java
@@ -0,0 +1,4 @@
+package com.example.crud.controller;
+
+public record CreateUserDto(String username, String email, String password) {
+}
---
+++ src/main/java/com/example/crud/controller/UpdateUserDto.java
@@ -0,0 +1,4 @@
+package com.example.crud.controller;
+
+public record UpdateUserDto(String username, String password) {
+}
