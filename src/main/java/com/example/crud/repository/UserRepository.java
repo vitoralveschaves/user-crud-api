dd9de2f711711c62a997f6b4ad859a229a5c009a
vitoralveschaves <vitorchavesalv@gmail.com>
2024-07-15 01:08:56 -0300

    feat: add UserRepository

---
+++ src/main/java/com/example/crud/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.crud.repository;
+
+import com.example.crud.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
